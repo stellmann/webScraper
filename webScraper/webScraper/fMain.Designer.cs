6677c4d8739d3cf3a895ba8221411e3457f1300c
fstellmann <f.stellmann@protonmail.com>
2019-09-13 11:57:02 +0200

    First version

---
+++ webScraper/webScraper/fMain.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace webScraper
+{
+    partial class fMain
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbxURL = new System.Windows.Forms.TextBox();
+            this.btnWork = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // tbxURL
+            // 
+            this.tbxURL.Location = new System.Drawing.Point(12, 12);
+            this.tbxURL.Name = "tbxURL";
+            this.tbxURL.Size = new System.Drawing.Size(324, 20);
+            this.tbxURL.TabIndex = 0;
+            // 
+            // btnWork
+            // 
+            this.btnWork.Location = new System.Drawing.Point(342, 10);
+            this.btnWork.Name = "btnWork";
+            this.btnWork.Size = new System.Drawing.Size(75, 23);
+            this.btnWork.TabIndex = 1;
+            this.btnWork.Text = "Scrape";
+            this.btnWork.UseVisualStyleBackColor = true;
+            this.btnWork.Click += new System.EventHandler(this.btnWork_Click);
+            // 
+            // fMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(427, 42);
+            this.Controls.Add(this.btnWork);
+            this.Controls.Add(this.tbxURL);
+            this.Name = "fMain";
+            this.Text = "webScraper";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbxURL;
+        private System.Windows.Forms.Button btnWork;
+    }
+}
+
---
+++ webScraper/webScraper/fMain.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Web;
+using System.Net;
+using System.IO;
+
+namespace webScraper
+{
+    public partial class fMain : Form
+    {
+        public fMain()
+        {
+            InitializeComponent();
+        }
+
+        private void btnWork_Click(object sender, EventArgs e)
+        {
+            if(String.IsNullOrEmpty(tbxURL.Text))
+            {
+                MessageBox.Show("Please enter an URL in the textbox.");
+            }
+            else
+            {
+                Uri u= new Uri(tbxURL.Text);
+                if(!u.IsWellFormedOriginalString())
+                {
+                    MessageBox.Show("Please enter an valid URL in the textbox.");
+                    tbxURL.Clear();
+                }
+                else
+                {
+                    HttpWebRequest request = (HttpWebRequest)WebRequest.Create(u);
+                    HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+                    string responseString = new StreamReader(response.GetResponseStream()).ReadToEnd();
+                    File.WriteAllText(@".\output.html", responseString);
+                    MessageBox.Show("Scraping complete.");
+                    tbxURL.Clear();
+                }
+            }
+        }
+    }
+}
